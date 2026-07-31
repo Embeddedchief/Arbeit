27cc9cf925ee3985c6ef6db4f7f8a133698f3cc2
embeddedchief <samuelsetemijp@gmail.com>
2026-07-31 19:09:54 +0100

    #login_screen.dart

--- lib/features/authentication/screens/login_screen.dart
+++ lib/features/authentication/screens/login_screen.dart
@@ -11,7 +11,50 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Image.asset("assets/images/logo.png")]),
+      body: Column(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        //Why my widget didnt get centralized in a column with crossAxis set to center
+        children: [
+          SizedBox(height: 152),
+          Center(child: Image.asset("assets/images/logo.png")),
+          SizedBox(height: 15),
+          Text("Login to your account"),
+
+          Padding(
+            padding: const EdgeInsets.all(26.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "Email Address",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                        color: Color(0Xff0056D2),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                TextFormField(
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Color(0Xff0056D2)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
